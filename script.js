471148e18ea1e062827f0e481805a25c7fafe2e0
Mark Lawson <mark@walkonmars.co.uk>
2021-08-06 14:07:52 +0100

    working on JS

--- script.js
+++ script.js
@@ -3,78 +3,103 @@ document.addEventListener("DOMContentLoaded", () => {
   // First create an array of objects to hold the card images
   const cardArray = [
     {
-      card: "black-lab",
+      nom: "black-lab",
       img: "images/black-lab.jpg",
     },
 
     {
-      card: "choc-lab",
+      nom: "choc-lab",
       img: "images/choc-lab.jpg",
     },
 
     {
-      card: "dalmation",
+      nom: "dalmation",
       img: "images/dalmation.jpg",
     },
 
     {
-      card: "french-bulldog",
+      nom: "french-bulldog",
       img: "images/french-bulldog",
     },
 
     {
-      card: "pup-glasses",
+      nom: "pup-glasses",
       img: "images/pup-glasses",
     },
 
     {
-      card: "weimaraner",
+      nom: "weimaraner",
       img: "images/weimaraner.jpg",
     },
 
     {
-      card: "black-lab",
+      nom: "black-lab",
       img: "images/black-lab.jpg",
     },
 
     {
-      card: "choc-lab",
+      nom: "choc-lab",
       img: "images/choc-lab.jpg",
     },
 
     {
-      card: "dalmation",
+      nom: "dalmation",
       img: "images/dalmation.jpg",
     },
 
     {
-      card: "french-bulldog",
+      nom: "french-bulldog",
       img: "images/french-bulldog.jpg",
     },
 
     {
-      card: "pup-glasses",
+      nom: "pup-glasses",
       img: "images/pup-glasses.jpg",
     },
 
     {
-      card: "weimaraner",
+      nom: "weimaraner",
       img: "images/weimaraner.jpg",
     },
   ];
 
-  //Create the game board
-  // first get the .grid element using query selector
+  //get the .grid element using query selector
   const grid = document.querySelector(".grid");
+  //create empty array for cards chosen
+  let cardsChosen = [];
+  //and for the cardsChosen ID
+  let cardsChosenId = [];
 
-  //then loop over the cardArray and use createElement method to create the image elements
-  for (let i = 0; i < cardArray.length; i++) {
-    let card = document.createElement("img");
-    //use setAttribute to link to the relevant image path of question-mark card to start the game
-    card.setAttribute("src", "images/question-mark.png");
-    //use data-id and i to loop through the array and assign an index from 0 - 11
-    card.setAttribute("data-id", i);
-    //add an event listener to listen for when the card(s) are clicked, which will trigger the flipcard function
-    card.addEventListener("click", flipcard);
+  //Create the game board
+  function createBoard() {
+    //loop over the cardArray and use createElement method to create the image elements
+    for (let i = 0; i < cardArray.length; i++) {
+      let card = document.createElement("img");
+      //use setAttribute to link to the relevant image path of question-mark card to start the game
+      card.setAttribute("src", "images/question-mark.png");
+      //use data-id and i to loop through the array and assign an index from 0 - 11
+      card.setAttribute("data-id", i);
+      //add an event listener to listen for when the card is clicked, this will trigger the flipcard function
+      card.addEventListener("click", flipCard);
+
+      //place all cards onto the grid using appendChild method
+      grid.appendChild(card);
+    }
   }
+
+  //check for matches
+
+  //flip card function
+  function flipCard() {
+    //getAttribute gets the data-id created on line 81
+    let cardId = this.getAttribute("data-id");
+    //push the selected card, based on the cardId, into the cardsChosen array, storing it's name
+    cardsChosen.push(cardArray[cardId].nom);
+    //the cardId will also be stored in a separate array - cardsChosenId
+    cardsChosenId.push(cardId);
+    //use setAttribute to place the relevant image to the card square, depending on which cardId it holds
+    this.setAttribute("src", cardArray[cardId].img);
+  }
+
+  createBoard();
 });
