2cc04b4cd4354c929f5b6ab8a8ad0dfa472422bb
Mark Lawson <mark@walkonmars.co.uk>
2021-08-06 12:28:59 +0100

    cardArray declared in JS

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-// First create an array of objects to hold the card images
+document.addEventListener("DOMContentLoaded", () => {
+  // First create an array of objects to hold the card images
+  const cardArray = [
+    {
+      name: "black-lab",
+      img: "images/black-lab.jpg",
+    },
+
+    {
+      name: "choc-lab",
+      img: "images/choc-lab.jpg",
+    },
+
+    {
+      name: "dalmation",
+      img: "images/dalmation.jpg",
+    },
+
+    {
+      name: "french-bulldog",
+      img: "images/french-bulldog",
+    },
+
+    {
+      name: "pup-glasses",
+      img: "images/pup-glasses",
+    },
+
+    {
+      name: "weimaraner",
+      img: "images/weimaraner.jpg",
+    },
+  ];
+});
