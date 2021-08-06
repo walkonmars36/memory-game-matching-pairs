0fc91e3fa510c69c4a0037f19aa1ba9ef4e583c9
Mark Lawson <mark@walkonmars.co.uk>
2021-08-06 12:29:31 +0100

    linked js and css to index.html

--- script.js
+++ script.js
@@ -2,32 +2,32 @@ document.addEventListener("DOMContentLoaded", () => {
   // First create an array of objects to hold the card images
   const cardArray = [
     {
-      name: "black-lab",
+      card: "black-lab",
       img: "images/black-lab.jpg",
     },
 
     {
-      name: "choc-lab",
+      card: "choc-lab",
       img: "images/choc-lab.jpg",
     },
 
     {
-      name: "dalmation",
+      card: "dalmation",
       img: "images/dalmation.jpg",
     },
 
     {
-      name: "french-bulldog",
+      card: "french-bulldog",
       img: "images/french-bulldog",
     },
 
     {
-      name: "pup-glasses",
+      card: "pup-glasses",
       img: "images/pup-glasses",
     },
 
     {
-      name: "weimaraner",
+      card: "weimaraner",
       img: "images/weimaraner.jpg",
     },
   ];
